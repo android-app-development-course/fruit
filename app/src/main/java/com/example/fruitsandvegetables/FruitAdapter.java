28d3c7758e702923a205b34bfbbd207fc565a40d
Paul <1329522082@qq.com>
2017-12-28 22:53:31 +0800

    1228 add 识别训练集 以及 优化了一些原有UI

--- app/src/main/java/com/example/fruitsandvegetables/FruitAdapter.java
+++ app/src/main/java/com/example/fruitsandvegetables/FruitAdapter.java
@@ -22,6 +22,7 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder>{
 
     private List<Fruit> mFruitList;
 
+    boolean isShow=false;
     static class ViewHolder extends RecyclerView.ViewHolder {
         CardView cardView;
         ImageView fruitImage;
@@ -50,13 +51,25 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder>{
         final ViewHolder holder = new ViewHolder(view);
         holder.cardView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) {//点击item出现详细信息。
+                isShow=false;
                 int position = holder.getAdapterPosition();
                 Fruit fruit = mFruitList.get(position);
                 Intent intent = new Intent(mContext, FruitActivity.class);
                 intent.putExtra(FruitActivity.FRUIT_NAME, fruit.getName());
                 intent.putExtra(FruitActivity.FRUIT_IMAGE_ID, fruit.getImageId());
                 mContext.startActivity(intent);
+            }
+        });
+        holder.cardView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {//长按item删除功能
+                isShow=true;
+                int position=holder.getAdapterPosition();
+                mFruitList.remove(position);
+                notifyItemRemoved(position);
+                notifyItemRangeChanged(position,mFruitList.size());
+                return false;
             }
         });
         return holder;
--- app/src/main/java/com/example/fruitsandvegetables/MainActivity.java
+++ app/src/main/java/com/example/fruitsandvegetables/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.fruitsandvegetables;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -27,10 +33,8 @@ import java.util.Random;
 
 public class MainActivity extends BaseActivity {
 
-
-
     private SwipeRefreshLayout swipeRefreshLayout;
-
+    private DBHelper myDatebaseHelper;
     private DrawerLayout mdrawerLayout;
 
 
@@ -48,7 +52,8 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        myDatebaseHelper = new DBHelper(this, "DAILY.db", null, 1);
+        SQLiteDatabase db = myDatebaseHelper.getWritableDatabase();
         initFruits();
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
@@ -109,6 +114,7 @@ public class MainActivity extends BaseActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         Toast.makeText(MainActivity.this,"Added Successfully",Toast.LENGTH_SHORT).show();
+                        openFriutCard();
                     }
                 });
                 builder.setNegativeButton("No,No,No", new DialogInterface.OnClickListener() {
@@ -121,6 +127,7 @@ public class MainActivity extends BaseActivity {
                 builder.show();
 
                 Snackbar.make(v,"Fruit Added",Snackbar.LENGTH_SHORT).setAction("Undo",
+
                         new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
@@ -171,6 +178,41 @@ public class MainActivity extends BaseActivity {
                         Toast.makeText(MainActivity.this,"This is MyAccount page",Toast.LENGTH_SHORT).show();
                         mdrawerLayout.closeDrawers();
                         break;
+
+                    case R.id.object_identification:
+                        //待添加
+                        mdrawerLayout.closeDrawers();
+                        break;
+                    case R.id.object_library:
+                        //待添加
+                        mdrawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.nav_share:
+                        //待添加
+                        mdrawerLayout.closeDrawers();
+                        break;
+                    case R.id.nav_contact_us:
+                        AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+                        builder1.setTitle("Contact us")
+                                .setMessage("please phone 18316299722")
+                                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Toast.makeText(MainActivity.this,"soon coming...",Toast.LENGTH_SHORT).show();
+                                    }
+                                })
+                                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Toast.makeText(MainActivity.this,"Do nothing",Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                                builder1.create();
+                                builder1.show();
+                        mdrawerLayout.closeDrawers();
+                        break;
+
                         default:
                             break;
                 }
@@ -209,14 +251,36 @@ public class MainActivity extends BaseActivity {
 
 
     }
-
+    //界面跳转，跳到水果卡界面
+    private void openFriutCard(){
+        Intent intent =new Intent();
+        intent.setClass(MainActivity.this,FruitCard.class);
+        startActivity(intent);
+    }
     private void initFruits() {
         fruitList.clear();
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 20; i++) {
             Random random = new Random();
             int index = random.nextInt(fruits.length);
             fruitList.add(fruits[index]);
         }
+    }
+
+    public void Query(){
+        SQLiteDatabase db = myDatebaseHelper.getWritableDatabase();
+        Cursor cursor = db.query("Photo", null, null, null, null, null, null);
+
+        while (cursor.moveToNext()) {
+            byte[] in = cursor.getBlob(cursor.getColumnIndex("fruitimage"));
+            long did=cursor.getLong(cursor.getColumnIndex("_id"));
+            String name = cursor.getString(cursor.getColumnIndex("fruitname"));
+            Bitmap bmpout = BitmapFactory.decodeByteArray(in, 0, in.length);
+            Drawable pic=chage_to_drawable(bmpout);
+            Fruit Nfruit = new Fruit(name,pic,did);
+            fruitList.add(Nfruit);
+        }
+        cursor.close();
+        db.close();
     }
 
     private void refreshFruits() {
@@ -232,6 +296,7 @@ public class MainActivity extends BaseActivity {
                     @Override
                     public void run() {
                         initFruits();
+                        Query();
                         adapter.notifyDataSetChanged();
                         swipeRefreshLayout.setRefreshing(false);
                     }
@@ -246,6 +311,13 @@ public class MainActivity extends BaseActivity {
         return true;
     }
 
+    public Drawable chage_to_drawable(Bitmap bp)
+    {
+        //因为BtimapDrawable是Drawable的子类，最终直接使用bd对象即可。
+        Bitmap bm=bp;
+        BitmapDrawable bd= new BitmapDrawable(this.getResources(), bm);
+        return bd;
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
@@ -255,6 +327,10 @@ public class MainActivity extends BaseActivity {
                 break;
             case R.id.backup:
                 Toast.makeText(this,"You clicked Backup",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.addfirut1:
+                Toast.makeText(this,"You clicked AddNew",Toast.LENGTH_SHORT).show();
+                openFriutCard();
                 break;
             case R.id.delete:
                 Toast.makeText(this,"You clicked Delete",Toast.LENGTH_SHORT).show();
