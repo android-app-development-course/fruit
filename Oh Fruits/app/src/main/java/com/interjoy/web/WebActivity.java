d7e73af268405e52799c9e96a8381318545d9517
Paul <1329522082@qq.com>
2018-01-10 15:16:10 +0800

    最终版本

---
+++ Oh Fruits/app/src/main/java/com/interjoy/web/WebActivity.java
@@ -0,0 +1,15 @@
+package com.interjoy.web;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.interjoy.FruitsIdentification.R;
+
+public class WebActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+    }
+}
